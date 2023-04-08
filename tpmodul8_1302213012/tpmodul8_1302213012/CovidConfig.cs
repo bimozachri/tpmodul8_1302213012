87d815cedcc9d72667f8841078a14cd7eb09db1c
Bimo Zachriansyah Wicaksono Hermawan <bimozachri@gmail.com>
2023-04-09 06:14:24 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_1302213012/tpmodul8_1302213012/CovidConfig.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8_1302213012
+{
+    internal class CovidConfig
+    {
+        public Config config;
+
+        private const string filepath = @"covid_config.json";
+
+        public CovidConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch
+            {
+                SetDefault();
+                WriteConfigFile();
+            }
+        }
+
+        public void ReadConfigFile()
+        {
+            string baca = File.ReadAllText(filepath);
+            config = JsonSerializer.Deserialize<Config>(baca);
+        }
+
+        public void WriteConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            string write = JsonSerializer.Serialize(config, options);
+            File.WriteAllText(filepath, write);
+        }
+
+        public void SetDefault()
+        {
+            config = new Config();
+
+            config.satuan_suhu = "celcius";
+            config.batas_hari_demam = 14;
+            config.pesan_ditolak = "anda tidak diperbolehkan masuk ke dalam gedung ini";
+            config.pesan_diterima = "anda dipersilahkan untuk masuk ke dalam gedung ini";
+        }
+
+        public void UbahSatuan()
+        {
+            string baca = File.ReadAllText(filepath);
+            config = JsonSerializer.Deserialize<Config>(baca);
+
+            if (config.satuan_suhu.Contains("celcius"))
+            {
+                config.satuan_suhu = "fahrenheit";
+            }
+            else if (config.satuan_suhu.Contains("fahrenheit"))
+            {
+                config.satuan_suhu = "celcius";
+            }
+
+            string newJson = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
+
+            File.WriteAllText(filepath, newJson);
+        }
+    }
+
+    public class Config
+    {
+        public string satuan_suhu { get; set; }
+        public int batas_hari_demam { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+        public Config() { }
+
+        public Config(string satuan_suhu, int batas_hari_demam, string pesan_ditolak, string pesan_diterima)
+        {
+            this.satuan_suhu = satuan_suhu;
+            this.batas_hari_demam = batas_hari_demam;
+            this.pesan_ditolak = pesan_ditolak;
+            this.pesan_diterima = pesan_diterima;
+        }
+    }
+}
--- tpmodul8_1302213012/tpmodul8_1302213012/Program.cs
+++ tpmodul8_1302213012/tpmodul8_1302213012/Program.cs
@@ -1,2 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul8_1302213012;
+
+CovidConfig covConfig = new CovidConfig();
+covConfig.UbahSatuan();
+
+Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai " + covConfig.config.satuan_suhu);
+string temperatureTubuh = Console.ReadLine();
+int temperature = (int)Convert.ToInt32(temperatureTubuh);
+
+Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+string dayIn = Console.ReadLine();
+int day = (int)Convert.ToInt32(dayIn);
+
+if (covConfig.config.satuan_suhu == "celcius")
+{
+    if (temperature > 37.5)
+    {
+        if (day > covConfig.config.batas_hari_demam)
+        {
+            Console.WriteLine(covConfig.config.pesan_ditolak);
+        }
+        else
+        {
+            Console.WriteLine(covConfig.config.pesan_diterima);
+        }
+    }
+    else
+    {
+        Console.WriteLine(covConfig.config.pesan_diterima);
+    }
+}
+else if (covConfig.config.satuan_suhu == "fahrenheit")
+{
+    if (temperature > 99.5)
+    {
+        if (day > covConfig.config.batas_hari_demam)
+        {
+            Console.WriteLine(covConfig.config.pesan_ditolak);
+        }
+        else
+        {
+            Console.WriteLine(covConfig.config.pesan_diterima);
+        }
+    }
+    else
+    {
+        Console.WriteLine(covConfig.config.pesan_diterima);
+    }
+}
